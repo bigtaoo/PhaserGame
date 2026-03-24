717e2f693ff7285c01ad861291e7ba06ec5aa7c4
王涛 <tao.wang.go@gmail.com>
2026-03-24 21:55:17 +0100

    android.

---
+++ android/app/src/main/java/com/taowang/sumquest/MainActivity.java
@@ -0,0 +1,5 @@
+package com.taowang.sumquest;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.taowang.sumquest',
+  appName: 'SumQuest',
+  webDir: 'dist',
+};
+
+export default config;
